aa47bfb0f4ba6390894b6d7001d8ce839e005dcc
bekzod mirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2021-12-15 13:06:52 +0500

    master

---
+++ app.js
@@ -0,0 +1,57 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.container')
+
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+upBtn.addEventListener('click', () => {
+    changeSlide("up")
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide("down")
+})
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up')
+    }else if (event.key === 'ArrowDown') {
+        changeSlide('down')
+    }
+})
+
+document.querySelector('body').addEventListener('wheel',(event)=>{
+    if(event.deltaY < 0) {
+        changeSlide('down')
+    }else if(event.deltaY > 0) {
+        changeSlide('up')
+    }
+})
+
+    
+    
+
+
+function changeSlide (direction) {
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0
+        }
+    }else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+    const height = container.clientHeight
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
